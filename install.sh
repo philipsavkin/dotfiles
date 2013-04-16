ee451fa0c983e5ee5344b5503635f2372027e0e6
Philip Savkin <philip.savkin@gmail.com>
2013-04-16 18:37:19 +0300

    gitconfig

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 DOTFILES_DIR=`pwd`
 
-ln -s $DOTFILES_DIR/gitconfig ~/.gitconfig
+cp $DOTFILES_DIR/gitconfig ~/.gitconfig
 ln -s $DOTFILES_DIR/zshrc ~/.zshrc
 ln -s $DOTFILES_DIR/tmux.conf ~/.tmux.conf
 ln -s $DOTFILES_DIR/vimrc ~/.vimrc
