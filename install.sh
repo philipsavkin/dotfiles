51f9b4ffe15e18ccb053fafb14a07957e16846a2
Philip Savkin <philip.savkin@gmail.com>
2014-03-13 22:36:24 +0200

    gemrc added

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 cp $DOTFILES_DIR/gitconfig ~/.gitconfig
 ln -s $DOTFILES_DIR/zshrc ~/.zshrc
+ln -s $DOTFILES_DIR/gemrc ~/.gemrc
 ln -s $DOTFILES_DIR/tmux.conf ~/.tmux.conf
 ln -s $DOTFILES_DIR/vimrc ~/.vimrc
 ln -s $DOTFILES_DIR/vim ~/.vim
