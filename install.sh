4c147b6f0f3969f017778f002010412d877db0a9
Philip Savkin <philip.savkin@gmail.com>
2014-03-16 14:05:20 +0200

    rspec config added

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 cp $DOTFILES_DIR/gitconfig ~/.gitconfig
 ln -s $DOTFILES_DIR/zshrc ~/.zshrc
 ln -s $DOTFILES_DIR/gemrc ~/.gemrc
+ln -s $DOTFILES_DIR/rspec ~/.rspec
 ln -s $DOTFILES_DIR/tmux.conf ~/.tmux.conf
 ln -s $DOTFILES_DIR/vimrc ~/.vimrc
 ln -s $DOTFILES_DIR/vim ~/.vim
