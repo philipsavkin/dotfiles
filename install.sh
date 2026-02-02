9bf4c0aa229ebd376e822293f985dfce38aabbc2
philip <filips.savkins@hypercell.games>
2026-02-02 18:34:09 +0200

    add starship config

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-cp $DOTFILES_DIR/.gitconfig ~/.gitconfig
+cp $DOTFILES_DIR/.gitconfig ~/.gitconfig.new
 #ln -s $DOTFILES_DIR/zshrc ~/.zshrc
 ln -s $DOTFILES_DIR/.config/nvim ~/.config/nvim
+ln -s $DOTFILES_DIR/.config/starship.toml ~/.config/starship.toml
